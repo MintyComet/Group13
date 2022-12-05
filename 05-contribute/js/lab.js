8b18343b0026eee9e21e184a4d7db64eb8cd72a6
kmwashin <64337304+kmwashin@users.noreply.github.com>
2022-12-05 06:17:01 -0800

    thats it
    stores posts locally so post stay after refreshing. search bar highlights more. hopefully. fixed css hopefully so itll look the same on every devicceeeeeeee.

--- 05-contribute/js/lab.js
+++ 05-contribute/js/lab.js
@@ -1,9 +1,5 @@
-document.getElementById("my-button").addEventListener("click", function() {
-    var max = document.getElementById("user-input").value;
-      $("#output").append("<p>" + max + "</p>");
 
-
-  });
+//Nav bar code
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -15,14 +11,51 @@ document.getElementById("my-button").addEventListener("click", function() {
     document.getElementById("main").style.marginLeft = "0";
   }
 
+//searchbar
   function search(){
       let textToSearch = document.getElementById("search").value;
       let paragraph = document.getElementById("paragraph");
+      let paragraph1 = document.getElementById("paragraph1");
+      //let paragraph3 = document.getElementById("ul");
+
 
 
       textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
 
       let pattern = new RegExp(`${textToSearch}`,"gi");
 
-      paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+      paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`);
+      paragraph1.innerHTML = paragraph1.textContent.replace(pattern, match => `<mark>${match}</mark>`);
+    //  paragraph3.innerHTML = paragraph3.textContent.replace(pattern, match => `<mark>${match}</mark>`);
+
+
   }
+
+  //setting up for localStorage
+  const form = document.querySelector('form');
+  const ul = document.querySelector('ul');
+  //const button = document.querySelector('button');
+  const input = document.getElementById('user-input');
+  let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  const data = JSON.parse(localStorage.getItem('items'));
+
+  const liMaker = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+  }
+
+  document.getElementById("my-button").addEventListener("click", function () {
+    //e.preventDefault();
+
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    liMaker(input.value);
+    input.value = "";
+  });
+
+  data.forEach(item => {
+    liMaker(item);
+  });
