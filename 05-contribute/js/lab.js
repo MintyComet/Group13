ca520a9ed71610a7d10b6e5edc72986389be9be5
kmwashin <64337304+kmwashin@users.noreply.github.com>
2022-12-04 23:37:49 -0800

    advice
    omgggggggggggggggggggggggggg that sucked

--- 05-contribute/js/lab.js
+++ 05-contribute/js/lab.js
@@ -14,3 +14,16 @@ document.getElementById("my-button").addEventListener("click", function() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+  function search(){
+      let textToSearch = document.getElementById("search").value;
+      let paragraph = document.getElementById("paragraph");
+
+
+      textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
+
+      let pattern = new RegExp(`${textToSearch}`,"gi");
+
+      paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+  }
+  
