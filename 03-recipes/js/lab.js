d2cf69412d7a4ce90db0e5da776e4c0268fb0a6d
MatthewOropeza <114529622+MatthewOropeza@users.noreply.github.com>
2022-12-05 00:55:01 -0800

    hwoiu

--- 03-recipes/js/lab.js
+++ 03-recipes/js/lab.js
@@ -11,27 +11,69 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://recipesapi2.p.rapidapi.com/recipes/ramen?maxRecipes=10",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "0ab5cd73damsha9977e2dd4af9fap1b12d6jsn551315d06ffc",
-		"X-RapidAPI-Host": "recipesapi2.p.rapidapi.com"
-	}
-};
+recipeName.addEventListener("input", (e) => {
+    // inside, we will need to achieve a few things:
+    // 1. declare and assign the value of the event's target to a variable AKA whatever is typed in the search bar
+    let search = e.target.value
 
-fetch("https://recipesapi2.p.rapidapi.com/recipes/")
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
+    // 2. check: if input exists and if input is larger than 0
+    if (value && value.trim().length > 0){
+        // 3. redefine 'value' to exclude white space and change input to all lowercase
+         value = value.toLowerCase()
+        // 4. return the results only if the value of the search is included in the person's name
+        // we need to write code (a function for filtering through our data to include the search input value)
     } else {
-      throw new Error("NETWORK RESPONSE ERROR");
+      console.log("Error:", textStatus, errorThrown);
+        // 5. return nothing
+        // input is invalid -- show an error message or show no results
+
     }
-  })
-  .then(data => {
-    console.log(data);
-    displayRecipes(data)
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
+
+}
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://recipesapi2.p.rapidapi.com/recipes/"+ encodeURIComponent(search),
+    // The data to send (will be converted to a query string)
+    data: {
+            // here is where any data required by the api
+            //   goes (check the api docs)
+            X-RapidAPI-Key: '0ab5cd73damsha9977e2dd4af9fap1b12d6jsn551315d06ffc',
+            X-RapidAPI-Host: 'recipesapi2.p.rapidapi.com'
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+})
+
+function setList(results){
+
+    for (const person of results){
+        // creating a li element for each result item
+        const resultItem = document.createElement('li')
+
+        // adding a class to each item of the results
+        resultItem.classList.add('result-item')
+
+        // grabbing the name of the current point of the loop and adding the name as the list item's text
+        const text = document.createTextNode(person.name)
+
+        // appending the text to the result item
+        resultItem.appendChild(text)
+
+        // appending the result item to the list
+        list.appendChild(resultItem)
+    }
+}
